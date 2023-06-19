53d602c904d7b3307650f5d183ab276c1efff456
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2023-06-19 19:42:45 +0400

    feat: added new functions

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const routes = require('./routes');
 
-const { PORT = 3000 } = process.env
-
+const { PORT = 3000 } = process.env;
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Слишком много запросов с данного IP, повторите попытку позднее',
+});
+
+app.use(limiter);
+app.use(helmet());
 app.use(express.json());
-
-
-/**временное решение авторизации */
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '647efda413ce7cf48ab11682',
   };
 
   next();
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-app.use(router);
-app.use('/', (reg, res) => {
-  res.status(404).send({ message: 'Что-то пошло не так'});
-});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true });
+
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`порт ${PORT}`)
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
 });
