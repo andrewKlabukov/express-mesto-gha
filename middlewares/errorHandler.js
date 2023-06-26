5af3998d3e69e5bbfe19b5a238559726500258b1
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2023-06-26 19:51:45 +0400

    14

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,9 @@
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+    message: statusCode === 500 ? 'Ошибка сервера' : message,
   });
   next();
 };
+
+module.exports = errorHandler;
