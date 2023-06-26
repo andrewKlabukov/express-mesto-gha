c617f46081c187f25ca949790a3d73917ff1d70f
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2023-06-26 23:26:15 +0400

    14

--- errors/ForbiddenError.js
+++ errors/ForbiddenError.js
@@ -1,4 +1,4 @@
-const { STATUS_CODES } = require('../constants');
+const { STATUS_CODES } = require('../utils/constants');
 
 module.exports = class ForbiddenError extends Error {
   constructor(message) {
