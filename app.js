c59ab4f0cdbf975f31b8d31270e9e74d2dc23567
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2023-06-29 19:24:39 +0400

    14

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// ограничение кол-ва запросов (защита от DoS-атак)
-const rateLimit = require('express-rate-limit');
-// защита приложения от некоторых веб-уязвимостей
+
 const helmet = require('helmet');
+
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes');
+
 const errorHandler = require('./middlewares/errorHandler');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // "окно" - 15 минут
-  max: 100, // ограничить каждый IP-адрес 100 запросами за "окно" (за 15 минут)
+  windowMs: 15 * 60 * 1000,
+  max: 100,
 });
 
 mongoose.connect('mongodb://127.0.0.1/mestodb');
